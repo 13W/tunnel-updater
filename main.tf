25b0a467704fc4a4c7a6fad43afa70d675fd30d4
Vladimir Bulyga <zero@13w.me>
2018-12-09 18:24:04 +0300

    ~

--- main.tf
+++ main.tf
@@ -1,27 +1,9 @@
-data "template_file" "script" {
-  template = <<END
-cd /app
-apt-get update
-apt-get install -y curl gcc
-
-curl https://sh.rustup.rs -sSf | sh -s -- \
-  -y \
-  --default-host x86_64-unknown-linux-gnu \
-  --default-toolchain nightly-x86_64-unknown-linux-gnu 
-source /root/.cargo/env
-cargo install cargo-deb
-cargo deb
-END
-  vars {
-
-  }
-}
-
 variable "images" {
   type = "list"
   default = [
     "stretch",
-    "wheezy"]
+    "wheezy"
+  ]
 }
 
 provider "docker" {}
@@ -37,12 +19,30 @@ resource "docker_container" "build" {
   command             = [
     "bash",
     "-c",
-    "${data.template_file.script.rendered}"
+    <<END
+      cd /app
+      apt-get update
+      apt-get install -y curl gcc
+      
+      curl https://sh.rustup.rs -sSf | sh -s -- \
+        -y \
+        --default-host x86_64-unknown-linux-gnu \
+        --default-toolchain nightly-x86_64-unknown-linux-gnu 
+      source /root/.cargo/env
+      cargo install cargo-deb
+      cargo deb
+      mkdir -p target/debian-${var.images[count.index]}
+      cp target/debian/* target/debian-${var.images[count.index]}
+END
   ]
 
   volumes {
     container_path    = "/app"
     host_path         = "${path.cwd}"
+  }
+
+  volumes {
+    container_path    = "/app/target/debian"
   }
 
   provisioner "local-exec" {
